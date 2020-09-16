32bbafddcbe7b48215d6a6d6721cd9ccd2fa191c
natalyapilkevich <nataly.pilkevich@yandex.ru>
2020-09-16 16:50:14 +0300

    Подготавливает к сдаче

--- source/js/script.js
+++ source/js/script.js
@@ -1,23 +1,20 @@
 'use strict';
 
 (function () {
-  var headerNav = document.querySelector('.header__nav');
+  var header = document.querySelector('.header');
   var navToggle = document.querySelector('.header__nav button');
-  var logo = document.querySelector('.header svg');
 
-  headerNav.classList.remove('header__nav--nojs');
-  headerNav.classList.remove('header__nav--opened');
-  headerNav.classList.add('header__nav--closed');
+  header.classList.remove('header--nojs');
+  header.classList.remove('header--opened');
+  header.classList.add('header--closed');
 
   navToggle.addEventListener('click', function () {
-    if (headerNav.classList.contains('header__nav--closed')) {
-      headerNav.classList.remove('header__nav--closed');
-      headerNav.classList.add('header__nav--opened');
-      logo.style.stroke = '#011C40';
+    if (header.classList.contains('header--closed')) {
+      header.classList.remove('header--closed');
+      header.classList.add('header--opened');
     } else {
-      headerNav.classList.add('header__nav--closed');
-      headerNav.classList.remove('header__nav--opened');
-      logo.style.stroke = '#F9FBFD';
+      header.classList.add('header--closed');
+      header.classList.remove('header--opened');
     }
   });
 })();
